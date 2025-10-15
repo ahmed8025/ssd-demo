091647b48dc1d78f2088b1469e3e60e74386b96e
ahmed8025 <i227441@nu.edu.pk>
2025-10-15 22:08:45 +0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,6 @@
-echo "
---- Compiling c ---
-"
-gcc HelloWorld.c -o hw
-if [ $? -ne 0 ]; then
-echo "ERROR: c compilation failed.
-exit 1
-"
-fi
-echo "
---- Executing c Program ---
-./hw
-echo "c Build and Test SUCCESS"
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
